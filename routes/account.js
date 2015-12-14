b86bd6e5e05ba952e63409f1540c03956793adc5
bhuber2010 <bhuber@alumni.colostate.edu>
2015-12-13 21:40:22 -0700

    update account route

--- routes/account.js
+++ routes/account.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { title: 'Express', user: req.user }));
+router.get('/', ensureAuthenticated, function(req, res){
+  res.render('account', { title: 'Express', user: req.user });
 });
 
 module.exports = router;
